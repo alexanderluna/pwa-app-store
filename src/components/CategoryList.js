b172e842cd8686b0065db60bc9cfb5eb0133f491
Mycroft1891 <mycroft1891@gmail.com>
2018-07-24 16:19:58 +0200

    Add: component for categories

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 class CategoryList extends Component {
   render() {
-    return (
-      <h1>Category View</h1>
+    const { categories } = this.props;
+    return(
+      <div>
+        <Typography variant="subheading">Browse by Category</Typography>
+        <div style={{display: 'inline-flex', maxWidth: '90vw', overflow: 'scroll'}}>
+          { categories.map((category, id) =>
+            <Button
+              key={id}
+              href={`/pwa-app-store/category/${category}`}
+              style={{ margin: '1rem 0.5rem' }}
+              variant="outlined"
+              color="primary"
+              size="large" >
+              { category }
+            </Button>
+          )}
+        </div>
+      </div>
     )
   }
 }
 
-export default CategoryList
+export default CategoryList;
