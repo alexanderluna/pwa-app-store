0a9417915b9e8476c0c07fd1cddb10aa098e1465
Mycroft1891 <mycroft1891@gmail.com>
2018-07-31 21:08:29 +0200

    Add: detail view and styling

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,28 +1,85 @@
 import React, { Component } from 'react';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Button from '@material-ui/core/Button';
 import { getById } from '../services/Project'
 
 class Detail extends Component {
 
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      project: [],
+      tabIndex: 0
+    }
+    this.handleTab = this.handleTab.bind(this);
   }
-
   componentWillMount() {
     const project = getById(this.props.match.params.id);
-    this.setState({ project: project});
+    this.setState({ project });
+  }
+  handleTab(event, tabIndex) {
+    this.setState({ tabIndex });
   }
   render() {
-    const { id, name, description, image, author, link, category } = this.state.project;
+    const { name, description, image, author, link, category } = this.state.project;
+    const { tabIndex } = this.state;
     return (
       <div>
-        <h1>{ name }</h1>
-        <p>{ category }</p>
-        <p>{ id }</p>
-        <p>{ description }</p>
-        <p>{ image }</p>
-        <p>{ author }</p>
-        <p>{ link }</p>
+        <Card className="detailCard">
+          <CardMedia
+            className="detailImage"
+            image={image}
+            title="lorem ipsum"
+          />
+          <Typography
+            className="detailName"
+            variant="title">
+            {name}
+          </Typography>
+          <Typography
+            className="detailAuthor"
+            variant="subheading">
+            by {author.substring(0,15)}
+          </Typography>
+          <Button
+            className="detailInstall"
+            variant="contained"
+            color="primary"
+            href="#">
+            Install
+          </Button>
+          <Typography
+            className="detailDescription"
+            variant="body2">
+            {description}
+          </Typography>
+          <div className="cardFooter">
+            <Button size="small" color="secondary">{category}</Button>
+          </div>
+        </Card>
+
+        <Paper>
+          <Tabs
+            value={tabIndex}
+            onChange={this.handleTab}
+            indicatorColor="primary"
+            textColor="primary"
+            centered
+          >
+            <Tab label="iOS" />
+            <Tab label="Android" />
+            <Tab label="Windows Mobile" />
+          </Tabs>
+        </Paper>
+
+        {tabIndex === 0 && <h1>For iOS</h1>}
+        {tabIndex === 1 && <h1>For Android</h1>}
+        {tabIndex === 2 && <h1>For Windows Mobile</h1>}
       </div>
     )
   }
